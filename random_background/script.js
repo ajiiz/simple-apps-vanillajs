ad6b7de4b82c86bf6d36c5a513156c82aa3b390b
ajiiz <opticpiter@gmail.com>
2020-11-11 20:27:41 +0100

    Randomized the generated color more and added conditional font color rendering depending on color lightness

--- random_background/script.js
+++ random_background/script.js
@@ -2,10 +2,17 @@ const btn = document.getElementById("btn");
 const paragraph = document.getElementById("hsl");
 
 btn.addEventListener("click", () => {
-    let h = Math.floor(Math.random()* 356).toString();
-    const s = "75";
-    const l = "42";
+    const h = Math.floor(Math.random()* 356).toString();
+    const s = (Math.floor(Math.random()* 100) + 1).toString();
+    const l = (Math.floor(Math.random()* 100) + 1).toString();
     let hsl = "hsl(" + h + "," + s + "%," + l + "%)";
+
+    if (l > 70) {
+        document.body.style.color = "#232B32";
+    } else {
+        document.body.style.color = "#fff";
+    }
+
     document.body.style.backgroundColor = hsl;
     paragraph.innerHTML = hsl;
 })
