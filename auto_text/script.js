5cb5ebc0010285529be8a7ee31cf1d2f050cfc8b
ajiiz <opticpiter@gmail.com>
2020-11-14 21:41:53 +0100

    Added script that creates text

---
+++ auto_text/script.js
@@ -0,0 +1,15 @@
+const textElement = document.getElementById("text");
+const text = "Calm Your Soul :) "
+let i = 0;
+
+const generateText = () => {
+    textElement.innerHTML = text.slice(0, i);
+
+    i++;
+
+    if (i > text.length) {
+        i = 0;
+    }
+}
+
+setInterval(generateText, 200);
