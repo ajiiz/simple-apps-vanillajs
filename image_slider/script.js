fc42110c8d259a4c89b3485a9b65ea8960b73114
ajiiz <opticpiter@gmail.com>
2020-11-17 13:08:30 +0100

    Added script

---
+++ image_slider/script.js
@@ -0,0 +1,14 @@
+const slide = document.getElementById("images");
+const images = document.querySelectorAll("img");
+
+let index = 0;
+
+const slideIn = () => {
+    index++;
+    if (index > images.length - 1) {
+        index = 0;
+    }
+    slide.style.transform = `translateX(-${index*300}px)`;
+}
+
+setInterval(slideIn, 5000);
