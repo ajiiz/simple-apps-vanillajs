9346c9110d0f986d8440e7f093d4a2cfebbc134d
ajiiz <opticpiter@gmail.com>
2020-11-17 14:03:18 +0100

    Added buttons click events so you can change photos on slider

--- image_slider/script.js
+++ image_slider/script.js
@@ -1,14 +1,29 @@
 const slide = document.getElementById("images");
 const images = document.querySelectorAll("img");
+const button_left = document.getElementById("btn_left");
+const button_right = document.getElementById("btn_right");
 
 let index = 0;
+let direction = 0;
 
 const slideIn = () => {
-    index++;
+    index = (direction) ? index + 1 : index - 1;
+
     if (index > images.length - 1) {
         index = 0;
+    } else if (index < 0) {
+        index = images.length - 1;
     }
+
     slide.style.transform = `translateX(-${index*400}px)`;
 }
 
-setInterval(slideIn, 5000);
+button_right.addEventListener('click', () => {
+    direction = 1;
+    slideIn();
+});
+
+button_left.addEventListener('click', () => {
+    direction = 0;
+    slideIn();
+});
