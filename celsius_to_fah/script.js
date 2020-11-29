4c12c570ddc0d234dcb5bbda56c82888bb93f31f
ajiiz <opticpiter@gmail.com>
2020-11-29 16:55:08 +0100

    Added script that converts C to F

---
+++ celsius_to_fah/script.js
@@ -0,0 +1,17 @@
+const conv = document.getElementById("conv");
+const result = document.getElementById("result");
+let elementsArray = document.querySelectorAll("input");
+
+conv.addEventListener("click",(e) => {
+    result.style.opacity = "0";
+    let celsius = elementsArray[0].value;
+    let fah = celsius*1.8000 + 32.00;
+    if (celsius === "") {
+        fah = "Insert a celsius temperature";
+    } else {
+        fah = "In fahrenheit: " + fah + "F";
+    }
+    result.innerHTML = fah;
+    result.style.opacity = "1";
+    e.preventDefault();
+})
